fe33b6d5cdc9c8df437413810e8cbf2e21fe3648
wraithy <wraith.andrew@gmail.com>
2015-06-11 14:54:36 +0200

    added random rules to the list

--- break.sh
+++ break.sh
@@ -1,4 +1,5 @@
 echo "Generating Masks"
 python preprocess_mask.py masks.masks > processed_masks.masks
 python compile_masks.py $MPPATH processed_masks.masks rules.rules
-$CATPATH -m 0 -r rules.rules -o cracked.txt hashes.hash 500-worst-passwords.txt
+cat random_matched_rules.rule >> rules.rules
+$CATPATH -m 0 -r rules.rules -o cracked.txt -g 10000 hashes.hash 500-worst-passwords.txt
