51681b0b5b5dcb7d40e2e6ebbd7d3a4344880288
Darryl Meyer <myrdar003@myuct.ac.za>
2015-06-11 11:54:07 +0200

    Added 2 more dictionaries
    Added cain and rockyou dictionaries

--- make_specific_dict.sh
+++ make_specific_dict.sh
@@ -1,3 +1,7 @@
 wget http://s3.amazonaws.com/wikia_xml_dumps/b/ba/battlefieldheroes_pages_current.xml.gz
 gzip -d battlefieldheroes_pages_current.xml.gz
+wget http://downloads.skullsecurity.org/passwords/rockyou.txt.bz2
+gzip -d rockyou.txt.bz2
+wget http://downloads.skullsecurity.org/passwords/cain.txt.bz2
+gzip -d cain.txt.bz2
 python parser.py
