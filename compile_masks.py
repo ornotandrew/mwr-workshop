7623db63e9da78ec6624023d18a664ee6cf5ac81
steveryb <steveryb@gmail.com>
2015-06-11 12:44:49 +0200

    Added mask maker

---
+++ compile_masks.py
@@ -0,0 +1,20 @@
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(description='Process a list of masks into rules')
+parser.add_argument('mask_processor_path',  type=str, help='Where Mask Processor is located on your machine')
+parser.add_argument('mask_path',  help='Where the masks are stored')
+parser.add_argument('output',  help='Where to output the rule file')
+args = parser.parse_args()
+
+with open(args.mask_path) as mask_file, open(args.output, "w") as rules_file:
+    outs = []
+    for line in mask_file:
+        line = line.strip()
+        if line[0] == "#" or line[0] == "":
+            next
+        mask = line
+        out = subprocess.check_output([args.mask_processor_path, mask])
+        outs.extend(out.split("\n"))
+    for line in outs:
+        rules_file.write(line + "\n")
--- make_specific_dict.sh
+++ make_specific_dict.sh
@@ -1,7 +1,7 @@
 wget http://s3.amazonaws.com/wikia_xml_dumps/b/ba/battlefieldheroes_pages_current.xml.gz
+python parser.py
 gzip -d battlefieldheroes_pages_current.xml.gz
 wget http://downloads.skullsecurity.org/passwords/rockyou.txt.bz2
 gzip -d rockyou.txt.bz2
 wget http://downloads.skullsecurity.org/passwords/cain.txt.bz2
 gzip -d cain.txt.bz2
-python parser.py
